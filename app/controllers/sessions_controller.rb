f3f03d1796be5b6e50d940ea78c32e9856f9124b
Scott Macfarlane <macfarlane.scott@gmail.com>
2012-12-14 15:01:19 -0900

    Remove old code from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,4 @@
 class SessionsController < ApplicationController
-  # def create
-  #   @user = User.find_with_omniauth(auth_hash)
-  #   @user = User.create_with_omniauth(auth_hash) if @user.nil?
-  # 
-  #   session[:user_id] = @user.id
-  # 
-  #   redirect_to '/'
-  # end
-  # 
-  # def destroy
-  #   reset_session
-  #   redirect_to root_url, :notice => 'Signed out!'
-  # end
   
   def create
     user = User.where(:provider => auth_hash['provider'], 
