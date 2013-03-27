1aa759e296fa2f6b59a8be2df5a1295085f1b1e0
Scott Macfarlane <macfarlane.scott@gmail.com>
2013-03-27 15:40:35 -0800

    Clean up maps show action

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,9 +2,6 @@ class MapsController < ApplicationController
   layout "simple"
   
   def show
-    @year = Date.today.beginning_of_year
     @cruises = Cruise.year(@year)
-    
-  end
-  
+  end  
 end
