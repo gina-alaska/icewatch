827ace4f07bca6dd74b72820f557b8bc82d23e7a
Scott Macfarlane <macfarlane.scott@gmail.com>
2013-03-28 16:38:35 -0800

    Fix production environment

--- config/initializers/sideqik.rb
+++ config/initializers/sideqik.rb
@@ -3,4 +3,7 @@ Sidekiq.configure_server do |config|
     chain.add Kiqstand::Middleware
   end
   #config.redis = { :namespace => 'icewatch' }
+  if Rails.env == 'production'
+    config.redis = { :url => 'redis://localhost:6379/12', :namespace => "icewatch_production" }
+  end
 end
