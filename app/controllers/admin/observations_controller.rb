c4886a8323c6f0fade83bc81d6eee1559eb9c60c
Scott Macfarlane <scott@gina.alaska.edu>
2013-12-04 12:21:08 -0900

    Clean up accept/reject routes

--- app/controllers/admin/observations_controller.rb
+++ app/controllers/admin/observations_controller.rb
@@ -15,56 +15,38 @@ class Admin::ObservationsController < AdminController
       format.html
     end
   end
-  
-  def approve
-    @observation = observation.where(accepted: false, id: params[:id]).first
-        
-    if(@observation)
-      if accepted?
-        @observation.accepted = true
-        @observation.save
-      else
-        @observation.delete
-      end
-    end
-    redirect_to admin_observations_url 
-  end
-  
+
   def accept
     @observation = observation.where(accepted: false, id: params[:id]).first
-    
-    if @observation.update_attribute(accepted: true)
-      redirect_to admin_observations_url
+
+    if @observation and @observation.update_attribute(:accepted, true)
+      flash[:notice] = "Accepted observation"
+      redirect_to admin_cruise_path(@observation.cruise)
     else
-      
+      if @observation.nil?
+        flash[:error] = "Unable to find observation or observation has already been approved"
+        redirect_to admin_cruises_path
+      else
+        flash[:error] = "Unable to update observation"
+        redirect_to admin_cruise_path(@observation.cruise)
+      end
     end
   end
   
   def reject
     @observation = observation.where(accepted: false, id: params[:id]).first
     
-    if @observation.delete
-      redirect_to 
+    if @observation and @observation.delete
+      flash[:notice] = "Rejected observation"
+      redirect_to admin_cruise_path(@observation.cruise)
     else
-      
-    end
-  end
-  
-  def approve_selected
-    @observations = Observation.where(accepted: false)
-  
-    if params[:cruise_id]
-      @observations = @observations.where(cruise_id: params[:cruise_id])
-    end
-    #Todo: Figure out why update_all isnt' working
-    #@observations.update_all(accepted: true)
-    
-    @observations.each{|obs| obs.update_attributes(accepted: true)}
-    
-    if params[:cruise_id]
-      redirect_to admin_cruise_url(params[:cruise_id])
-    else
-      redirect_to admin_observations_url
+      if @observation.nil?
+        flash[:error] = "Unable to find observation or observation has already been approved"
+        redirect_to admin_cruises_path
+      else
+        flash[:error] = "Something went wrong"
+        redirect_to admin_cruise_path(@observation.cruise)  
+      end
     end
   end
   
