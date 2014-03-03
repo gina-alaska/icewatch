2e02847ec142c27874ecc6bdb680119e4e334a05
Scott Macfarlane <scott@gina.alaska.edu>
2014-03-03 10:08:19 -0900

    Fix the seeds to not clobber existing entries.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-%w{
-biota_lookup
-cloud_lookup
-floe_size_lookup
-ice_lookup
-max_depth_lookup
-pattern_lookup
-surface_lookup
-open_water_lookup
-on_boat_location_lookup
-sediment_lookup
-snow_lookup
-topography_lookup
-visibility_lookup
-weather_lookup
-}.each do |file|
-  puts file
-  eval(file.camelcase).delete_all
-  YAML.load_file("db/#{file}s.yml").each do |record|
-    eval(file.camelcase).create( record )
+
+Dir.glob("db/*_lookups.yml").each do |seed|
+  puts seed
+  table = File.basename(seed).chomp("s.yml").camelcase.constantize
+  seed_data = YAML.load_file(seed)
+  seed_data.each do |data|
+    item = table.where(code: data[:code]).first_or_create 
+    item.update_attributes(data)
   end
 end
