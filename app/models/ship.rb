511434c2ef95059f9befe4e253406f0dd26332c7
Scott Macfarlane <scott@gina.alaska.edu>
2014-09-09 16:05:56 -0800

    Include assist-shared csv

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,10 +1,12 @@
 class Ship
   include Mongoid::Document
+  include AssistShared::CSV::Ship
+
   field :heading, type: Integer
   field :power, type: Integer
   field :speed, type: Integer
 
   belongs_to :ship_activity_lookup
-  
+
   embedded_in :observation
 end
