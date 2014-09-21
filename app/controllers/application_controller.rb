0b82c33292230ae73fe0eba64301f31895736bd7
Scott Macfarlane <scott@gina.alaska.edu>
2014-09-21 14:19:47 -0800

    Add gina template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   include GinaAuthentication::AppHelpers
+
+  before_action :set_variant_type
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def set_variant_type
+    request.vairant :assist if ENV['ICEWATCH_ASSIST']
+  end
 end
