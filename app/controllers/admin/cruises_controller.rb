b70a113ad2a98f170882ecadbadbfc2a83103285
Scott Macfarlane <scott@gina.alaska.edu>
2014-12-01 13:59:59 -0900

    Don't hide unapproved cruises in the admin page

--- app/controllers/admin/cruises_controller.rb
+++ app/controllers/admin/cruises_controller.rb
@@ -1,42 +1,42 @@
 class Admin::CruisesController < AdminController
   respond_to :html
   def index
-    @all_cruises = Cruise.only(:start_date)
-    @cruises = Cruise.desc(:approved).desc(:start_date,:end_date)
+    @all_cruises = Cruise.unscoped.only(:start_date)
+    @cruises = Cruise.unscoped.desc(:approved).desc(:start_date,:end_date)
     if params[:year]
       year = Time.utc(params[:year])
       @cruises = @cruises.between(start_date: [year, year.end_of_year])
     end
   end
-  
+
   def new
     @cruise = Cruise.new
   end
-  
+
   def show
-    @cruise = Cruise.where(id: params[:id]).first
+    @cruise = Cruise.unscoped.where(id: params[:id]).first
     @pending = @cruise.observations.where(accepted: false)
     @observations = @cruise.observations
     @uploaded_observation = UploadedObservation.new
-    
+
     respond_to do |format|
       format.html { render layout: false if request.xhr?}
     end
   end
-  
-  def create 
+
+  def create
     @cruise = Cruise.new cruise_params
-    
+
     if @cruise.save
       redirect_to admin_cruise_url @cruise
     else
       render action: :new
     end
   end
-  
+
   def approve
     @cruise = Cruise.find(params[:id])
-    
+
     if @cruise
       if approved?
         @cruise.approved = true
@@ -53,11 +53,11 @@ class Admin::CruisesController < AdminController
       end
     end
   end
-  
+
   def accept_observations
     @cruise = cruise.where(id: params[:id]).first
     @observations = observation.where(cruise_id: @cruise.id).in(id: params[:observation_ids])
-    
+
     @observations.update_all(accepted: true)
   end
   def reject_observations
@@ -72,7 +72,7 @@ protected
   def cruise_params
     params.slice(:cruise)
   end
-  
+
   def approved?
     params[:value] == "yes" ? true : false
   end
