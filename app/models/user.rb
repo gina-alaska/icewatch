f8a4325c1e0fd6e9e57ae07a40ac50fedf51e134
Scott Macfarlane <scott@gina.alaska.edu>
2014-12-18 10:29:14 -0900

    Add roles to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   include GinaAuthentication::UserModel
+  ROLES = %w{admin manager member guest}
+
+  def has_role?(base)
+    ROLES.index(base.to_s) >= ROLES.index(role)
+  end
 end
