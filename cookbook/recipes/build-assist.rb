5fe0c09b12d9a310482f64769be76f2f2b389ff8
Scott Macfarlane <scott@gina.alaska.edu>
2015-01-07 16:52:30 -0900

    Add jruby support for exporting stand-alone app

--- cookbook/recipes/build-assist.rb
+++ cookbook/recipes/build-assist.rb
@@ -1,14 +1,14 @@
-include_recipe 'java::default'
-
+# include_recipe 'java::default'
+#
 node.set['chruby']['rubies'] = {
-  'jruby' => true,
+  'jruby-1.7.18' => true,
   '1.9.3-p392' => false
 }
-node.set['chruby']['default'] = 'jruby'
-
-include_recipe 'chruby::system'
-
-package 'zip'
+node.set['chruby']['default'] = 'jruby-1.7.18'
+#
+# include_recipe 'chruby::system'
+#
+# package 'zip'
 
 gem_package 'bundler' do
   gem_binary ::File.join('/opt/rubies', node['chruby']['default'], 'bin/gem')
@@ -16,7 +16,13 @@ end
 
 #Database config
 
-rails_env = {"RAILS_ENV" => 'production'}
+rails_env = {
+  "RAILS_ENV" => 'production',
+  "SECRET_KEY_BASE" => '31f071a9f7a37810dbfaa253b07c972be761a1fe90db090fb97382c12f84d8f0d0216ca9feddb430dc95c0e48f79b6a0136c6664e5a07d067dc7305b9b1e4fd3',
+  "ICEWATCH_ASSIST" => "true",
+  "JRUBY_OPTS" => '--2.0'
+}
+
 chruby_exec = "chruby-exec #{node['chruby']['default']} --"
 
 execute 'bundle-install' do
@@ -38,7 +44,7 @@ execute 'precompile-assets' do
   env rails_env
 end
 
-execute 'warble-executeable-war' do
+execute 'warble-executable-war' do
   command "#{chruby_exec} bundle exec warble executable war"
   cwd node['icewatch']['paths']['application']
   env rails_env
@@ -47,6 +53,6 @@ end
 
 execute 'create-distributable-zip' do
   command "zip assist.war"
-  cwd node['iceatch']['paths']['application']
+  cwd node['icewatch']['paths']['application']
   creates 'assist.zip'
 end
