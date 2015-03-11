2a8e3030633991dd79f6c1b7ffc47e7ef84f7a4a
Scott Macfarlane <scott@gina.alaska.edu>
2015-03-11 09:38:47 -0800

    Remove legacy admin page

--- config/routes.rb
+++ config/routes.rb
@@ -29,13 +29,6 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get '/admin', to: 'admin/cruises#index'
-  namespace :admin do
-    resources :cruises, shallow: true do
-      resources :observations
-    end
-  end
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
