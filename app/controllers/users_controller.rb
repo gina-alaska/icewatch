78a4632427b856b41e38126d31286398b131f2b6
Scott Macfarlane <scott@gina.alaska.edu>
2015-05-08 09:48:44 -0800

    Add basic users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
   include GinaAuthentication::Users
+  before_action :setup_user, only: [:show]
+  def show
+
+  end
+
+  private
+  def setup_user
+    @user = User.find(params[:id])
+  end
 end
