7eb8d06f3b1ada1d026ddfdbbfd9fc249c8e3e3d
Scott Macfarlane <scott@gina.alaska.edu>
2015-06-30 09:53:55 -0800

    Must use absolute path, sans 'observations'

--- exports/launcher.windows.bat
+++ exports/launcher.windows.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set DB=%~dp0/production.sqlite3
-set EXPORT=observations
+set EXPORT=%~dp0
 set JAR=%~dp0/ASSIST.war
 set SECRET_KEY_BASE='31f071a9f7a37810dbfaa253b07c972be761a1fe90db090fb97382c12f84d8f0d0216ca9feddb430dc95c0e48f79b6a0136c6664e5a07d067dc7305b9b1e4fd3'
 set ICEWATCH_ASSIST='true'
