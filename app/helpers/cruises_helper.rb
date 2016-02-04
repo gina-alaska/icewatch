3f247b78a218b3437402f786ad48a43d0a71f3f2
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-04 09:22:20 -0900

    Add links to cruise metadata

--- app/helpers/cruises_helper.rb
+++ app/helpers/cruises_helper.rb
@@ -18,4 +18,12 @@ module CruisesHelper
   def ogre_url
     Rails.application.secrets.ogre_url
   end
+
+  def list_group_item_for(data, label)
+    return if data.nil?
+    content_tag(:li, '', class: 'list-group-item') do
+      concat(content_tag(:strong, label))
+      concat(data)
+    end
+  end
 end
