5f843d52ff02bd0a9e84e58ce033713f6ae4a284
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-05 12:45:28 -0900

    Rework test to refresh the page for every combination

--- test/integration/assist_workflow_test.rb
+++ test/integration/assist_workflow_test.rb
@@ -5,21 +5,33 @@ class AssistWorkflowTest < ActionDispatch::IntegrationTest
     logout
   end
 
+  # http://stackoverflow.com/questions/23371911/capybara-integration-tests-with-jquery-selectize
+  def fill_in_selectize(key, value)
+    page.execute_script(%{
+      $('#{key}').data('selectize').setValue(#{value});
+      })
+  end
+
   def test_user_cant_enter_floe_size_with_tencode_ice_types
     observation = observations(:base)
     login(:manager)
 
-    visit(edit_observation_path(observation))
-
-    within('#page') do
-      click_link("Ice")
-    end
-
     observation.ice_observations.each do |ice_observation|
-      within("#ice_observation_#{ice_observation.id}") do
-        %w{frazil shuga grease slush brash}.each do |ice_type|
-          select(lookups(ice_type.to_sym).code_with_name, from: "Ice Type")
-          assert_equal true, find('input[id=observation_ice_observations_attributes_0_id]')[:disabled]
+      %w{frazil shuga grease slush brash}.each do |ice_type|
+        visit(edit_observation_path(observation))
+
+        within('#page') do
+          click_link("Ice")
+        end
+
+        within("#ice_observation_#{ice_observation.id}") do
+          value = lookups(ice_type.to_sym).id
+          fill_in_selectize("#ice_observation_#{ice_observation.id} .ice-type", value)
+
+          assert has_css?('div.floe-size[disabled]'), "Floe Size div should be disabled for #{ice_observation.obs_type} #{ice_type}"
+          # FIXME:  Capybara is unable to find select.floe-size in the dom, yet
+          # it exists when looking at it in the debugger. I'm probably missing something obvious.
+          # assert has_css?('select.floe-size[readonly]'), "Floe Size select should be readonly for #{ice_observation.obs_type} #{ice_type}"
         end
       end
     end
