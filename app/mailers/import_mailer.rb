69ec98f15122968e36d0d5eef8049bb0029c1342
Scott Macfarlane <scott@gina.alaska.edu>
2016-02-29 10:40:59 -0900

    Simplify and add new-users

--- app/mailers/import_mailer.rb
+++ app/mailers/import_mailer.rb
@@ -1,11 +1,9 @@
 class ImportMailer < ApplicationMailer
   default from: 'admin@icewatch.gina.alaska.edu'
 
-  def daily_summary(email)
-    cruise_ids = Observation.saved.pluck(:cruise_id).uniq
-    return if cruise_ids.empty?
-
-    @cruises = Cruise.where(id: cruise_ids)
+  def daily_summary(email, cruises, users)
+    @users = users
+    @cruises = cruises
 
     mail(to: email, subject: 'IceWatch Daily Summary')
   end
