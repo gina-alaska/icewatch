bbc722670f9b10f0b604ed53de9a6d9129424360
Scott Macfarlane <scott@gina.alaska.edu>
2016-03-04 16:25:24 -0900

    Login manager before testing

--- test/controllers/uploaded_photosets_controller_test.rb
+++ test/controllers/uploaded_photosets_controller_test.rb
@@ -6,6 +6,7 @@ class UploadedPhotosetsControllerTest < ActionController::TestCase
   end
 
   test "should create uploaded_photoset" do
+    login_user(users(:manager))
     assert_difference('UploadedPhotoset.count') do
       post :create, cruise_id: cruises(:cruise_0), uploaded_photoset: { file: fixture_file_upload('images/vegas.jpg', 'image/jpg') }
     end
