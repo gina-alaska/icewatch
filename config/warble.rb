973f1d258e23a90ee9626b1ea9457a256d643e3d
Scott Macfarlane <macfarlane.scott@gmail.com>
2016-07-20 10:39:04 -0800

    Update bytecode_version

--- config/warble.rb
+++ config/warble.rb
@@ -94,7 +94,7 @@ Warbler::Config.new do |config|
   # config.compile_gems = false
 
   # When set it specify the bytecode version for compiled class files
-  # config.bytecode_version = "1.6"
+  config.bytecode_version = "1.7"
 
   # When set to true, Warbler will override the value of ENV['GEM_HOME'] even it
   # has already been set. When set to false it will use any existing value of
