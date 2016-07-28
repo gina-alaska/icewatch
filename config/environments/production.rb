981a9d4e4a7528d6879aa4f0bf5ec1e601755e1f
Scott Macfarlane <macfarlane.scott@gmail.com>
2016-07-28 14:30:43 -0800

    Add sidekiq

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Use sidekiq in production
+  config.active_job.queue_adapter = :sidekiq
 end
