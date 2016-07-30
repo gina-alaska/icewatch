5fac1be0ce6f3d666f8ff9d2bcc6e397511a2cfc
Scott Macfarlane <macfarlane.scott@gmail.com>
2016-07-30 15:09:39 -0800

    Fix erb in coffee (#121)

--- app/helpers/topographies_helper.rb
+++ app/helpers/topographies_helper.rb
@@ -1,2 +1,9 @@
 module TopographiesHelper
+  def concentration_ids
+    TopographyLookup.codes_between(200,499).pluck(:id)
+  end
+
+  def ridge_ids
+    TopographyLookup.codes_between(500,599).pluck(:id)
+  end
 end
