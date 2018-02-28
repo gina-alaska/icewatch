5812e4a07c9b5a1d22efaa93453792464b61a7b3
Rawser Spicer <rwspicer@alaska.edu>
2018-02-28 14:26:03 -0900

    added mailers for the new data, and new crusies. They still need to be updated with data.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,10 +3,21 @@ class AdminMailer < ApplicationMailer
   ## this emails all admins by default
   default to: Proc.new { User.where(role: 'admin').pluck(:email) }
   
-  def new_user(user)
+  def new_user (user)
     ## email for when when a new account is created
     @user = user
     mail(subject: 'New User Registered')
   end
-
+  
+  def new_cruise (cruise, user)
+    @cruise = cruise
+    @user = user
+    mail(subject: 'New Cruise Registered')
+  end
+  
+  def new_data_upload (data)
+    @cruise = data
+    mail(subject: 'New Data Uploaded')
+  end
+  
 end
--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -3,4 +3,12 @@ class AdminMailerPreview < ActionMailer::Preview
   def new_user
     AdminMailer.new_user(User.first)
   end
+  
+  def new_cruise
+    AdminMailer.new_cruise(Cruise.first, User.first)
+  end
+  
+  def new_data_upload
+    AdminMailer.new_data_upload()
+  end
 end
